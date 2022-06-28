dcf8c7415503073b21022aa335089667e7a43b31
Laura Smith <laurajayne8@live.com>
2022-06-28 21:07:30 +0100

    testing looping over the array

--- checkout.js
+++ checkout.js
@@ -1,6 +1,14 @@
 function checkout(array) {
-  return 2.0;
-  //   setting to 2.0 to pass one of the three tests, to check the tests
+  let A = 0;
+  let B = 0;
+  let C = 0;
+
+  for (const item of array) {
+    console.log(item);
+    // checking I can iterate over an array
+  }
 }
+
+checkout(["A", "B"]);
 
 module.exports = checkout;
