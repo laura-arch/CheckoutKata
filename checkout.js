39e322ddd5eec5baa601a28faee008459b412422
Laura Smith <laurajayne8@live.com>
2022-06-28 21:21:12 +0100

    implemented deals, tests now all passing

--- checkout.js
+++ checkout.js
@@ -2,7 +2,9 @@ function checkout(array) {
   let A = 0;
   let B = 0;
   let C = 0;
+  let cost = 0;
 
+  //   Sums total of each type of product:
   for (const item of array) {
     console.log(item);
     switch (item) {
@@ -17,7 +19,22 @@ function checkout(array) {
         break;
     }
   }
-  return 0.5 * A + 0.75 * B + 0.25 * C;
+
+  if (Math.floor(B / 2) > 0) {
+    cost += 1.25 * Math.floor(B / 2);
+    //   charging for the deals
+    B -= 2 * Math.floor(B / 2);
+    //   removing those we've already charged for
+  }
+
+  if (Math.floor(C / 4) > 0) {
+    cost += 0.75 * Math.floor(C / 4);
+    //   charging for the deals
+    C -= 4 * Math.floor(C / 4);
+    // removing those we've already charged for
+  }
+
+  return cost + 0.5 * A + 0.75 * B + 0.25 * C;
   //   checking we can sum the total without the discounts implemented
 }
 
